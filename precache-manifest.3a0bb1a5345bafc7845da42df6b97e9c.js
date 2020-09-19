14157179c4804929347e202aeb037d045fd6afda
Thomas Geissl <thomas.geissl@gmail.com>
2020-09-19 10:31:18 +0200

    Updates

---
+++ precache-manifest.3a0bb1a5345bafc7845da42df6b97e9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e03a4eaf128338ca025545eb32d439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92bc3aa910e8b175d4e3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "856115f9bf94c11a9107",
+    "url": "/static/js/2.ca91f58e.chunk.js"
+  },
+  {
+    "revision": "28e787dc3e4f56088bd949e004808b2c",
+    "url": "/static/js/2.ca91f58e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92bc3aa910e8b175d4e3",
+    "url": "/static/js/main.06d8e7fd.chunk.js"
+  },
+  {
+    "revision": "3a8681f3cd4bc9058b9e",
+    "url": "/static/js/runtime-main.23f04c3d.js"
+  },
+  {
+    "revision": "5e794cdbf0c31956db2378174155494d",
+    "url": "/static/media/A.5e794cdb.mp3"
+  }
+]);
