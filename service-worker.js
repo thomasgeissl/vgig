0e689f50b79b13e830a522cb2ee3a5ebc264979e
Thomas Geissl <thomas.geissl@gmail.com>
2020-10-31 14:26:53 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vgig/precache-manifest.86f2d2a34045f1d1e5715b026b454841.js"
+  "/vgig/precache-manifest.01bba28d5089d1552e283fc164dd9515.js"
 );
 
 self.addEventListener('message', (event) => {
